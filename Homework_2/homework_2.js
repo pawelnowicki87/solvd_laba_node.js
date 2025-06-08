b2da8912c75243bacd1c6ce553f7924c7d93dc49
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-06-08 22:01:37 +0200

    add task nr 2

---
+++ Homework_2/homework_2.js
@@ -0,0 +1,72 @@
+const DataTransformLib = {
+  addValues: function (a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      return a + b;
+    } else if (typeof a === 'string' || typeof b === 'string') {
+      return String(a) + String(b);
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+      return a.concat(b);
+    } else if (typeof a === 'object' && typeof b === 'object') {
+      return { ...a, ...b };
+    } else {
+      throw new Error('Values are incorrect');
+    }
+  },
+
+  stringifyValue: function (value) {
+    if (typeof value === 'object' && value !== null) {
+      return JSON.stringify(value);
+    } else {
+      return String(value);
+    }
+  },
+
+  invertBoolean: function (value) {
+    if (typeof value !== 'boolean') {
+      throw new Error('Value needs to be boolean');
+    }
+    return !value;
+  },
+
+  convertToNumber: function (value) {
+    const num = typeof value === 'string' ? parseFloat(value) : Number(value);
+    if (isNaN(num)) {
+      throw new Error('Cannot do such operation');
+    }
+    return num;
+  },
+
+  coerceToType: function (value, type) {
+    switch (type) {
+      case 'string':
+        return String(value);
+      case 'number':
+        const num = Number(value);
+        if (isNaN(num)) throw new Error('Cannot do the operation');
+        return num;
+      case 'boolean':
+        return Boolean(value);
+      case 'object':
+        if (typeof value === 'object') return value;
+        try {
+          return JSON.parse(value);
+        } catch {
+          throw new Error('Cannot do the operation');
+        }
+      default:
+        throw new Error('Uknknown type');
+    }
+  },
+
+  convertToDate: function (value) {
+    const date = new Date(value);
+    if (isNaN(date.getTime())) {
+      throw new Error('Nie można przekonwertować na datę');
+    }
+    return date;
+  },
+
+  isInteger: function (value) {
+    return Number.isInteger(value);
+  }
+};
